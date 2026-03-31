f8d5310d1f2edcaea5a6eb37c60d7157faa2201d
Caioszleal <caiosz.leal@gmail.com>
2026-03-31 11:27:24 -0300

    Meu primeiro commit

---
+++ Meu_Primeiro_Projeto_Java/src/App.java
@@ -0,0 +1,5 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Começando os estudos de Java!!!");
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Projetos_Java
 Repositorio criado para postar projetos realizado na linguagem Java.
+
+Alterando o arquivo de forma local
